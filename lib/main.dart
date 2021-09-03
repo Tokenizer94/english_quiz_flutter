be909231cc27cdb8aaddb1310e8f1446a38136dc
tokenizer94 <mostafafahimi73@gmail.com>
2021-09-03 09:39:39 +0430

    EQF-2 _ task is done

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,32 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
-void main() {
+Future<void> main() async {
+  await startupSetup();
   runApp(MyApp());
+}
+
+Future<void> startupSetup() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  if (!kIsWeb) {
+    await SystemChrome.setPreferredOrientations(
+      [DeviceOrientation.portraitUp],
+    );
+    SystemChrome.setSystemUIOverlayStyle(
+      const SystemUiOverlayStyle(
+        statusBarColor: Colors.transparent,
+      ),
+    );
+  }
 }
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
       home: Container(),
     );
   }
